1d4d187d79fc65bf5533c54b3deefe5efcbcaa61
Jim Jagielski <jimjag@gmail.com>
2021-09-30 17:38:46 -0400

    Push to max speed and save some cycles

--- Software/JJ65c02/minios/acia.s
+++ Software/JJ65c02/minios/acia.s
@@ -38,15 +38,15 @@ acia_active:    .res 1
 ACIA_init:
     pha
     stz acia_active
-    ;lda #(ACIA_HARDWARE_RESET)
-    ;sta ACIA_STATUS
-    ;lda ACIA_DATA
+    lda #(ACIA_HARDWARE_RESET)
+    sta ACIA_STATUS
+    lda ACIA_DATA
     lda #(ACIA_PARITY_DISABLE | ACIA_ECHO_DISABLE | ACIA_TX_INT_DISABLE_RTS_LOW | ACIA_RX_INT_DISABLE | ACIA_DTR_LOW)
     sta ACIA_COMMAND
-    lda #(ACIA_STOP_BITS_1 | ACIA_DATA_BITS_8 | ACIA_CLOCK_INT | ACIA_BAUD_9600)
+    lda #(ACIA_STOP_BITS_1 | ACIA_DATA_BITS_8 | ACIA_CLOCK_INT | ACIA_BAUD_19200)
     sta ACIA_CONTROL
     lda ACIA_CONTROL
-    cmp #(ACIA_STOP_BITS_1 | ACIA_DATA_BITS_8 | ACIA_CLOCK_INT | ACIA_BAUD_9600)
+    cmp #(ACIA_STOP_BITS_1 | ACIA_DATA_BITS_8 | ACIA_CLOCK_INT | ACIA_BAUD_19200)
     bne @done
     lda #1
     sta acia_active
@@ -94,17 +94,18 @@ ACIA_read_byte:
 ;================================================================================
 
 ACIA_write_byte:
+    phx
+    tax
     pha
 @wait_txd_empty_char:
-    ;lda ACIA_STATUS
-    ;and #(ACIA_STATUS_TX_EMPTY)
-    ;beq @wait_txd_empty_char
-    ;pla
-    ;pha
-    sta ACIA_DATA
+    lda ACIA_STATUS
+    and #(ACIA_STATUS_TX_EMPTY)
+    beq @wait_txd_empty_char
+    stx ACIA_DATA
     lda #$01                            ; wait 1ms (more than 520us for 19200 baud)
     jsr LIB_delay1ms
     pla
+    plx
     rts
 
 ;================================================================================
