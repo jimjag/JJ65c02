324209eb9cd0de5bc9786a017300b79515d966a4
Jim Jagielski <jimjag@gmail.com>
2023-10-27 07:20:50 -0400

    First round of VGA on Pico PI

---
+++ Software/pico-code/vga_core/vga_core.h
@@ -0,0 +1,54 @@
+/**
+ *
+ * HARDWARE CONNECTIONS
+ *  - GPIO 16 ---> VGA Hsync
+ *  - GPIO 17 ---> VGA Vsync
+ *  - GPIO 18 ---> 750 ohm resistor ---> VGA Red
+ *  - GPIO 19 ---> 750 ohm resistor ---> VGA Blue
+ *  - GPIO 20 ---> 750 ohm resistor ---> VGA Green
+ *  - GPIO 21 ---> 1000 ohm resistor ---> VGA Intensity (bright)
+ *  - RP2040 GND ---> VGA GND
+ *
+ * RESOURCES USED
+ *  - PIO state machines 0, 1, and 2 on PIO instance 0
+ *  - DMA channels 0, 1, 2, and 3
+ *  - 153.6 kBytes of RAM (for pixel color data)
+ *
+ */
+#include <stdint.h>
+#include <stddef.h>
+
+// Give the I/O pins that we're using some names that make sense - usable in main()
+ enum vga_pins {HSYNC=16, VSYNC, RED_PIN, GREEN_PIN, BLUE_PIN, I_PIN};
+
+// We can only produce 16 (4-bit) colors, so let's give them readable names - usable in main()
+enum colors {BLACK, DARK_RED, DARK_GREEN, DARK_YELLOW, DARK_BLUE, DARK_MAGENTA, DARK_CYAN, GREY,
+            BLACK2, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE};
+
+// VGA primitives - usable in main
+void initVGA(void);
+void drawPixel(short x, short y, char color);
+void drawVLine(short x, short y, short h, char color);
+void drawHLine(short x, short y, short w, char color);
+void drawLine(short x0, short y0, short x1, short y1, char color);
+void drawRect(short x, short y, short w, short h, char color);
+void drawCircle(short x0, short y0, short r, char color);
+void drawCircleHelper( short x0, short y0, short r, unsigned char cornername, char color);
+void fillCircle(short x0, short y0, short r, char color);
+void fillCircleHelper(short x0, short y0, short r, unsigned char cornername, short delta, char color);
+void drawRoundRect(short x, short y, short w, short h, short r, char color);
+void fillRoundRect(short x, short y, short w, short h, short r, char color);
+void fillRect(short x, short y, short w, short h, char color);
+void drawChar(short x, short y, unsigned char c, char color, char bg, unsigned char size);
+void setCursor(short x, short y);
+void setTextColor(char c);
+void setTextColor2(char c, char bg);
+void setTextSize(unsigned char s);
+void setTextWrap(char w);
+void tft_write(unsigned char c);
+void writeString(char* str);
+
+void VGA_fillScreen(uint16_t color);
+
+void dma_memset(void *dest, uint8_t val, size_t num);
+void dma_memcpy(void *dest, void *src, size_t num);
---
+++ Software/pico-code/vga_core/vga_core_demo.c
@@ -0,0 +1,182 @@
+/**
+ *
+ * HARDWARE CONNECTIONS
+ *  - GPIO 16 ---> VGA Hsync
+ *  - GPIO 17 ---> VGA Vsync
+ *  - GPIO 18 ---> 750 ohm resistor ---> VGA Red
+ *  - GPIO 19 ---> 750 ohm resistor ---> VGA Blue
+ *  - GPIO 20 ---> 750 ohm resistor ---> VGA Green
+ *  - GPIO 21 ---> 1000 ohm resistor ---> VGA Intensity (bright)
+ *  - RP2040 GND ---> VGA GND
+ *
+ * RESOURCES USED
+ *  - PIO state machines 0, 1, and 2 on PIO instance 0
+ *  - DMA channels 0, 1, 2, and 3
+ *  - 153.6 kBytes of RAM (for pixel color data)
+ *
+ */
+
+// VGA graphics library
+#include "vga_core.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/dma.h"
+
+// Some globals for storing timer information
+volatile unsigned int time_accum = 0;
+unsigned int time_accum_old = 0;
+char timetext[40];
+
+// Timer interrupt
+bool repeating_timer_callback(struct repeating_timer *t) {
+
+    time_accum += 1;
+    return true;
+}
+
+
+int main() {
+
+    // Initialize stdio
+    stdio_init_all();
+
+    // Initialize the VGA screen
+    initVGA();
+
+    // circle radii
+    short circle_x = 0;
+
+    // color chooser
+    char color_index = 0;
+
+    // position of the disc primitive
+    short disc_x = 0;
+    // position of the box primitive
+    short box_x = 0;
+    // position of vertical line primitive
+    short Vline_x = 350;
+    // position of horizontal line primitive
+    short Hline_y = 250;
+
+    // Draw some filled rectangles
+    fillRect(64, 0, 176, 50, BLUE); // blue box
+    fillRect(250, 0, 176, 50, RED); // red box
+    fillRect(435, 0, 176, 50, GREEN); // green box
+
+    // Write some text
+    setTextColor(WHITE);
+    setCursor(65, 0);
+    setTextSize(1);
+    writeString("Raspberry Pi Pico");
+    setCursor(65, 10);
+    writeString("Graphics primitives demo");
+    setCursor(65, 20);
+    writeString("JJ65C02");
+    setCursor(250, 0);
+    setTextSize(2);
+    writeString("Time Elapsed:");
+
+    // Setup a 1Hz timer
+    struct repeating_timer timer;
+    add_repeating_timer_ms(-1000, repeating_timer_callback, NULL, &timer);
+
+    char video_buffer[32];
+    setTextColor2(WHITE, BLACK);
+    setTextSize(1);
+    for (int i=0; i<4; i++) {
+      for (int j=0; j<4; j++){
+        fillRect(i*70+20, 150+j*70, 60, 60, i+4*j);
+        setCursor(i*70+20, 150+j*70);
+        sprintf(video_buffer, "%2d", i+4*j);
+        writeString(video_buffer);
+      }
+    }
+    // first row of colors
+    setCursor(0*70+20, 200+0*70);
+    writeString("BLACK");
+    setCursor(1*70+20, 200+0*70);
+    writeString("BLACK2");
+    setCursor(2*70+20, 200+0*70);
+    writeString("DARK_GREEN");
+    setCursor(3*70+20, 200+0*70);
+    writeString("GREEN");
+    // second row of colors
+    setCursor(0*70+20, 200+1*70);
+    writeString("DARK_BLUE");
+    setCursor(1*70+20, 200+1*70);
+    writeString("BLUE");
+    setCursor(2*70+20, 200+1*70);
+    writeString("DARK_CYAN");
+    setCursor(3*70+20, 200+1*70);
+    writeString("CYAN");
+    // thrid row of colors
+    setCursor(0*70+20, 200+2*70);
+    writeString("DARK_RED");
+    setCursor(1*70+20, 200+2*70);
+    writeString("RED");
+    setCursor(2*70+20, 200+2*70);
+    writeString("DARK_YELLOW");
+    setCursor(3*70+20, 200+2*70);
+    writeString("YELLOW");
+    // fourth row of colors
+    setCursor(0*70+20, 200+3*70);
+    writeString("DARK_MAGENTA");
+    setCursor(1*70+20, 200+3*70);
+    writeString("MAGENTA");
+    setCursor(2*70+20, 200+3*70);
+    writeString("GREY");
+    setCursor(3*70+20, 200+3*70);
+    writeString("WHITE");
+
+    while(true) {
+
+        // Modify the color chooser
+        if (color_index ++ == 15) color_index = 0;
+
+        // A row of filled circles
+        fillCircle(disc_x, 100, 20, color_index);
+        disc_x += 35;
+        if (disc_x > 640) disc_x = 0;
+
+        // Concentric empty circles
+        drawCircle(320, 200, circle_x, color_index);
+        circle_x += 1;
+        if (circle_x > 130) circle_x = 0;
+
+        // A series of rectangles
+        drawRect(10, 300, box_x, box_x, color_index);
+        box_x += 5;
+        if (box_x > 195) box_x = 10;
+
+        // Random lines
+        drawLine(210+(rand()&0x7f), 350+(rand()&0x7f), 210+(rand()&0x7f),
+                 350+(rand()&0x7f), color_index);
+
+        // Vertical lines
+        drawVLine(Vline_x, 300, (Vline_x>>2), color_index);
+        Vline_x += 2;
+        if (Vline_x > 620) Vline_x = 350;
+
+        // Horizontal lines
+        drawHLine(400, Hline_y, 150, color_index);
+        Hline_y += 2;
+        if (Hline_y > 400) Hline_y = 240;
+
+        // Timing text
+        if (time_accum != time_accum_old) {
+            time_accum_old = time_accum;
+            fillRect(250, 20, 176, 30, RED); // red box
+            sprintf(timetext, "%d", time_accum);
+            setCursor(250, 20);
+            setTextSize(2);
+            writeString(timetext);
+        }
+
+        // A brief nap
+        sleep_ms(10);
+
+   }
+
+}
