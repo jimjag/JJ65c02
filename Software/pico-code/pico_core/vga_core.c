a1d47253f72ee1e8d6be03c914f0d71ce7194e34
Jim Jagielski <jimjag@gmail.com>
2023-12-20 08:16:45 -0500

    more timing adjs

--- Software/pico-code/pico_core/vga_core.c
+++ Software/pico-code/pico_core/vga_core.c
@@ -125,7 +125,7 @@ static uint memin_offset;
 static uint memin_sm;
 static PIO memin_pio;
 static uint memin_pio_irq;
-static unsigned char inbuf[2400];
+static unsigned char inbuf[10240];
 static unsigned char *endbuf = inbuf + sizeof(inbuf);
 volatile static unsigned char *rptr = inbuf;
 volatile static unsigned char *wptr = inbuf;
