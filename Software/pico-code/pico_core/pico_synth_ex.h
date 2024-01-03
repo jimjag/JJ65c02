0db5d94d0d52c54e2fc40cb5b5e0f1ee2634ff0f
Jim Jagielski <jimjag@gmail.com>
2024-01-03 09:53:17 -0500

    Update NOTICE Update clock freq for sound PWM

--- Software/pico-code/pico_core/pico_synth_ex.h
+++ Software/pico-code/pico_core/pico_synth_ex.h
@@ -28,7 +28,7 @@ typedef int16_t Q14; // Signed fixed-point number with 14-bit fractional part
 #define ONE_Q28 ((Q28) (1 << 28)) // 1.0 for Q28 type
 #define ONE_Q14 ((Q14) (1 << 14)) // 1.0 for type Q14
 #define PI ((float) M_PI) // Pi in float type
-#define FCLKSYS (120000000) // system clock frequency (Hz)
+#define FCLKSYS (250000000) // system clock frequency (Hz)
 #define FS (44100) // sampling frequency (Hz)
 #define FA (440.0F) // reference frequency (Hz)
 
