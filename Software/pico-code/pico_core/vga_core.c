dced6325dbb48975d6f67374065fd341f9cb0d10
Jim Jagielski <jimjag@gmail.com>
2024-02-14 06:21:41 -0500

    Bump to 6502 running at 5Mhz

--- Software/pico-code/pico_core/vga_core.c
+++ Software/pico-code/pico_core/vga_core.c
@@ -120,7 +120,7 @@ static uint memin_offset;
 static uint memin_sm;
 static PIO memin_pio;
 static uint memin_pio_irq;
-static unsigned char inbuf[10240];
+static unsigned char inbuf[5120];
 static unsigned char *endbuf = inbuf + sizeof(inbuf);
 volatile static unsigned char *rptr = inbuf;
 volatile static unsigned char *wptr = inbuf;
